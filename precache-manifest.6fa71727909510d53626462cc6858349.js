61b7d4cbbf10f5f5933fd454820bc7c9aa2aa5c3
Saulius <you@example.com>
2020-10-19 11:44:36 +0300

    Updates

---
+++ precache-manifest.6fa71727909510d53626462cc6858349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ecd6cd9bc08a10795605bcbb3218c8",
+    "url": "/ReactRedux-Project1-deployed/index.html"
+  },
+  {
+    "revision": "24d25bc933e491bee16a",
+    "url": "/ReactRedux-Project1-deployed/static/css/main.d34150e1.chunk.css"
+  },
+  {
+    "revision": "6e78312ba387ddc5fe7f",
+    "url": "/ReactRedux-Project1-deployed/static/js/2.ad7ca38b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactRedux-Project1-deployed/static/js/2.ad7ca38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d25bc933e491bee16a",
+    "url": "/ReactRedux-Project1-deployed/static/js/main.40da4b3b.chunk.js"
+  },
+  {
+    "revision": "ec136e8d885c24557c4b",
+    "url": "/ReactRedux-Project1-deployed/static/js/runtime-main.a089ff38.js"
+  }
+]);
